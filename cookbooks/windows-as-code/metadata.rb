5f6fc8640b8f167a7b3b1ece9a9e1e47d455fbe8
David Coate <coateds@localhost.localdomain>
2019-04-16 13:25:01 -0400

    Integrates Chocolately Cookbook

---
+++ cookbooks/windows-as-code/metadata.rb
@@ -0,0 +1,2 @@
+name 'windows-as-code'
+depends 'chocolatey'
--- cookbooks/windows-as-code/recipes/default.rb
+++ cookbooks/windows-as-code/recipes/default.rb
@@ -1,1 +1,10 @@
-directory "c:/scripts"
+directory "c:/scriptses"
+directory "c:/scriptees"
+directory "c:/scripteas"
+
+# include_recipe 'chocolatey::default'
+# 
+# chocolatey_package 'git' do
+#   options '--params /GitAndUnixToolsOnPath'
+#   # action :uninstall
+# end
